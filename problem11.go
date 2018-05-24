44b9d35e5fe06b3e2c45815cc3e4e75e230e5e69
André Durão <andreluisdurao@gmail.com>
2018-05-24 13:04:15 -0300

    Adding cursor Struct on problem11

--- problem11.go
+++ problem11.go
@@ -13,6 +13,13 @@ import (
 	"strings"
 )
 
+const Limit = 20
+
+type Cursor struct {
+	Row int
+	Col int
+}
+
 func readMatrix() [][]int {
 	file, _ := os.Open("problem11grid.txt")
 	fileScanner := bufio.NewScanner(file)
@@ -20,20 +27,54 @@ func readMatrix() [][]int {
 	index := 0
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
-		// fmt.Println(line)
 		numbers := strings.Split(line, " ")
 		for _, v := range numbers {
 			number, _ := strconv.Atoi(v)
 			matrix[index] = append(matrix[index], number)
 		}
-		fmt.Println(matrix[index])
 		index++
 	}
 
 	return matrix
 }
 
+// TODO: CREATE Struct CURSOR
+func seekByDirection(cursor Cursor, matrix [][]int) int {
+	largest := 0
+	// up right down left
+	directions := [][]int{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
+	fmt.Println("seekByDirection")
+	for _, direction := range directions {
+		result := directionHandler(cursor, matrix, direction)
+		if result > largest {
+			largest = result
+		}
+	}
+	return largest
+}
+
+// TODO: CREATE Struct CURSOR
+func directionHandler(cursor Cursor, matrix [][]int, direction []int) int {
+	multiple := matrix[cursor.Row][cursor.Col]
+	return multiple
+}
+
+func seek(matrix [][]int) {
+	fmt.Println("seek")
+	largest := 0
+	for i := 0; i < Limit; i++ {
+		for j := 0; j < Limit; j++ {
+			// TODO: CREATE Struct CURSOR
+			result := seekByDirection(Cursor{j, i}, matrix)
+			if result > largest {
+				largest = result
+			}
+		}
+	}
+}
+
 func main() {
 	fmt.Println("Problem 11")
-	readMatrix()
+	matrix := readMatrix()
+	seek(matrix)
 }
