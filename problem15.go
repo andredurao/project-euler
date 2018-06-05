339a4b2774dd5faeb6641045bdf54ca1623d2793
André Durão <andreluisdurao@gmail.com>
2018-06-04 22:03:40 -0300

    Adding popcount function

--- problem15.go
+++ problem15.go
@@ -13,6 +13,38 @@ import (
 	"fmt"
 )
 
+const GridSize = 20
+
+// https://en.wikipedia.org/wiki/Hamming_weight
+func popcount(x uint64) int {
+	const (
+		m1  = 0x5555555555555555
+		m2  = 0x3333333333333333
+		m4  = 0x0f0f0f0f0f0f0f0f
+		h01 = 0x0101010101010101
+	)
+	x -= (x >> 1) & m1
+	x = (x & m2) + ((x >> 2) & m2)
+	x = (x + (x >> 4)) & m4
+	return int((x * h01) >> 56)
+}
+
 func main() {
 	fmt.Println("Problem 15")
+	// The amount of edges is equals 2x the amount of vertices to create a path from
+	// source to target
+	// I know already that every "balanced path" reaches it's destination from before:
+	// https://stackoverflow.com/questions/3522417/binary-numbers-with-the-same-quantity-of-0s-and-1s
+	limit := uint64(1 << (GridSize * 2))
+	var i, count uint64 = 0, 0
+	fmt.Println(limit)
+
+	for i < limit {
+		// TODO: use concurrence to improve performance
+		if popcount(i) == GridSize {
+			count++
+		}
+		i++
+	}
+	fmt.Println(count)
 }
