04c84ec72b8eb148cb734847d711520e84ad2f39
André Durão <andreluisdurao@gmail.com>
2019-03-14 20:02:31 -0300

    Add problem15 solution

--- problem15.go
+++ problem15.go
@@ -7,6 +7,7 @@
 //   ╹    ╹  ┗━    ╹   ┗━━ ┗━━╸
 //
 // How many such routes are there through a 20×20 grid?
+// A nice explanation: http://code.jasonbhill.com/python/project-euler-problem-15/
 package main
 
 import (
@@ -15,36 +16,25 @@ import (
 
 const GridSize = 20
 
-// https://en.wikipedia.org/wiki/Hamming_weight
-func popcount(x uint64) int {
-	const (
-		m1  = 0x5555555555555555
-		m2  = 0x3333333333333333
-		m4  = 0x0f0f0f0f0f0f0f0f
-		h01 = 0x0101010101010101
-	)
-	x -= (x >> 1) & m1
-	x = (x & m2) + ((x >> 2) & m2)
-	x = (x + (x >> 4)) & m4
-	return int((x * h01) >> 56)
-}
-
 func main() {
 	fmt.Println("Problem 15")
-	// The amount of edges is equals 2x the amount of vertices to create a path from
-	// source to target
-	// I know already that every "balanced path" reaches it's destination from before:
-	// https://stackoverflow.com/questions/3522417/binary-numbers-with-the-same-quantity-of-0s-and-1s
-	limit := uint64(1 << (GridSize * 2))
-	var i, count uint64 = 0, 0
-	fmt.Println(limit)
 
-	for i < limit {
-		// TODO: use concurrence to improve performance
-		if popcount(i) == GridSize {
-			count++
-		}
-		i++
+	// initialize grid
+	grid := [GridSize + 1][GridSize + 1]int64{}
+
+	//initialize borders
+	for i := 0; i <= GridSize; i++ {
+		grid[0][i] = 1
+		grid[i][0] = 1
 	}
-	fmt.Println(count)
+
+	// Iterate through the grid elements
+	for i := 1; i <= GridSize; i++ {
+		for j := 1; j <= GridSize; j++ {
+			grid[i][j] = grid[i-1][j] + grid[i][j-1]
+		}
+	}
+
+	//The Result is the target position
+	fmt.Println(grid[GridSize][GridSize])
 }
