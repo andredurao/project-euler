83ee094bcc083fcd9baa872d78c53269dda942e0
André Durão <andreluisdurao@gmail.com>
2019-04-04 19:19:01 -0300

    Add problem19 solution

--- problem19.go
+++ problem19.go
@@ -14,8 +14,53 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	fmt.Println("Problem 19")
+	fmt.Println(seekUsingAPI())
+	fmt.Println(seekManually())
+}
+
+// monday = 0 ... sunday = 6
+func isSunday(day int) bool {
+	return day%7 == 6
+}
+
+func seekManually() int {
+	day, count := 0, 0
+	for year := 1900; year < 2001; year++ {
+		for month := 1; month <= 12; month++ {
+			if isSunday(day) && year > 1900 {
+				count++
+			}
+			day += daysInMonth(year, month)
+		}
+	}
+	return count
+}
+
+func daysInMonth(year int, month int) int {
+	var daysMap = map[int]int{1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30,
+		10: 31, 11: 30, 12: 31}
+	if month == 2 && ((year%4 == 0 && year%100 != 0) || (year%400 == 0)) {
+		return 29
+	} else {
+		return daysMap[month]
+	}
+}
+
+func seekUsingAPI() int {
+	count := 0
+	for year := 1901; year < 2001; year++ {
+		for month := 1; month <= 12; month++ {
+			// Using golang time API
+			date := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
+			if date.Weekday() == time.Sunday {
+				count++
+			}
+		}
+	}
+	return count
 }
