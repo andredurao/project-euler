0a82f0618bb7db06bf430c3a1139dfab8578cccf
André Durão <andreluisdurao@gmail.com>
2019-05-28 21:44:59 -0300

    Add Problem 47

--- problem47.go
+++ problem47.go
@@ -64,8 +64,60 @@ func primeFactors(n int, primes *[]int) (primeDivisors []int) {
 	}
 }
 
+func distinctPrimeFactors(n int, primes *[]int) (primeDivisors []int) {
+	number := n
+	i := 0
+	prime := (*primes)[i]
+	factorsMap := make(map[int]struct{})
+	for {
+		if i == len(*primes) {
+			*primes = append(*primes, nextPrime(prime))
+		}
+		prime = (*primes)[i]
+		if number%prime == 0 {
+			factorsMap[prime] = struct{}{}
+			number /= prime
+		} else {
+			i++
+		}
+		if number == 1 {
+			for k := range factorsMap {
+				primeDivisors = append(primeDivisors, k)
+			}
+			return
+		}
+	}
+}
+
+// This problems resembles a little like string search algorithms
+// the search for consecutive numbers could start at the suffix, like
+// Boyer-Moore algorithm
+func consecutiveSeeker(length int, primes *[]int) (index int) {
+	shift := 0
+	for {
+		for i := (index + length); i > index; i-- {
+			// p("index", index, "shift", shift, "i", i)
+			// p(distinctPrimeFactors(i, primes))
+			if len(distinctPrimeFactors(i, primes)) == length {
+				shift++
+			} else {
+				break
+			}
+		}
+		if shift == 0 {
+			index += length
+		} else if shift == length {
+			return
+		} else {
+			index += shift
+		}
+		shift = 0
+	}
+}
+
 func main() {
 	p("Problem 47")
 	primes := []int{2, 3, 5, 7}
-	p(primeFactors(1579, &primes))
+	result := consecutiveSeeker(4, &primes)
+	p("result", result+1)
 }
