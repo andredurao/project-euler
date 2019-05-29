504a56d241495c3956b7f08fd8b49a506f6ab20a
André Durão <andreluisdurao@gmail.com>
2019-05-29 19:56:08 -0300

    WIP Problem 49

---
+++ problem49.go
@@ -0,0 +1,94 @@
+// Prime permutations
+// The arithmetic sequence, 1487, 4817, 8147, in which each of the terms increases
+// by 3330, is unusual in two ways:
+// (i) each of the three terms are prime, and,
+// (ii) each of the 4-digit numbers are permutations of one another.
+// There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes,
+// exhibiting this property, but there is one other 4-digit increasing sequence.
+// What 12-digit number do you form by concatenating the three terms in this sequence?
+
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+var p = fmt.Println
+
+func isPrime(n int) bool {
+	max := int(math.Ceil(math.Sqrt(float64(n))))
+	var i int
+	if n == 1 {
+		return false
+	}
+	if n == 2 {
+		return true
+	}
+	for i = 2; i <= max; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func nextPrime(n int) (next int) {
+	next = n + 2
+	for !isPrime(next) {
+		next += 2
+	}
+	return
+}
+
+// pre build a map of primes with 4 digits
+func buildPrimesMap(primesMap map[int]struct{}) {
+	prime := 999
+	for {
+		prime = nextPrime(prime)
+		if prime < 10000 {
+			primesMap[prime] = struct{}{}
+		} else {
+			break
+		}
+	}
+}
+
+func primePermutations(n int) (permutations []int) {
+	digits := []rune(strconv.Itoa(n))
+	numberPermutations := make([]string, 0)
+	generatePermutations(digits, len(digits), &numberPermutations)
+	for _, value := range numberPermutations {
+		// p(number)
+		number, _ := strconv.Atoi(value)
+		if isPrime(number) {
+			permutations = append(permutations, number)
+		}
+	}
+
+	return
+}
+
+func generatePermutations(array []rune, n int, permutations *[]string) {
+	if n == 1 {
+		*permutations = append(*permutations, string(array))
+	} else {
+		for i := 0; i < n; i++ {
+			generatePermutations(array, n-1, permutations)
+			if n%2 == 0 {
+				array[0], array[n-1] = array[n-1], array[0]
+			} else {
+				array[i], array[n-1] = array[n-1], array[i]
+			}
+		}
+	}
+}
+
+func main() {
+	p("Problem 49")
+	primesMap := make(map[int]struct{})
+	buildPrimesMap(primesMap)
+	// p(primesMap)
+	p(primePermutations(1487))
+}
