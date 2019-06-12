a025edbc6fee21438e2217496832abae6e855041
André Durão <andreluisdurao@gmail.com>
2019-06-11 21:48:33 -0300

    WIP - Problem 60 finding pairs

--- problem60.go
+++ problem60.go
@@ -10,6 +10,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"strconv"
 )
 
 var p = fmt.Println
@@ -39,6 +40,22 @@ func nextPrime(n int) (next int) {
 	return
 }
 
+// AFAIK the qty of k-permutations for n is evaluated: n! / (n-k)!
+// ex: P(n,k) => P(5,2) = 5! / (5-2)! = 20
+func permutations(primes []int) (values []int) {
+	for i := 0; i < len(primes); i++ {
+		for j := 0; j < len(primes); j++ {
+			if i != j {
+				strValue := strconv.Itoa(primes[i])
+				strValue += strconv.Itoa(primes[j])
+				intValue, _ := strconv.Atoi(strValue)
+				values = append(values, intValue)
+			}
+		}
+	}
+	return
+}
+
 func generatePrimes(limit int) (map[int]struct{}, []int) {
 	primesList := make([]int, 0)
 	primesMap := make(map[int]struct{})
@@ -51,9 +68,35 @@ func generatePrimes(limit int) (map[int]struct{}, []int) {
 	return primesMap, primesList
 }
 
+func isSetComposedOfPrimes(primes []int, primesMap map[int]struct{}) bool {
+	for _, prime := range primes {
+		_, found := primesMap[prime]
+		if !found {
+			if !isPrime(prime) {
+				return false
+			}
+			primesMap[prime] = struct{}{}
+		}
+	}
+	return true
+}
+
+// WIP - Start looking for a pair of values that are in the same conditions
+func seekPairSets(primesList []int, primesMap map[int]struct{}) {
+	// start at 3,5 because permutations with 2 won't be valid
+	for i := 1; i < (len(primesList) - 1); i++ {
+		for j := i + 1; j < len(primesList); j++ {
+			currentSet := []int{primesList[i], primesList[j]}
+			primes := permutations(currentSet)
+			if isSetComposedOfPrimes(primes, primesMap) {
+				p(primes)
+			}
+		}
+	}
+}
+
 func main() {
 	p("Problem 60")
-	primesMap, primesList := generatePrimes(1000)
-	p(primesList)
-	p(primesMap)
+	primesMap, primesList := generatePrimes(100)
+	seekPairSets(primesList, primesMap)
 }
